d9e9abf72bb31bed3393995574d1c9b4a276facc
DennisOnder <webdev.dennisonder@gmail.com>
2019-04-14 21:11:21 +0200

    Nested describe statements, more tests

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-module.exports = () => {
-  return "hello";
+module.exports = {
+  sayHello: () => {
+    return "hello";
+  },
+  addNumbers: (val1, val2) => {
+    return val1 + val2;
+  }
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,25 @@
 const assert = require("chai").assert;
 const app = require("../index");
 
+// Results
+const sayHelloResult = app.sayHello();
+const addNumbersResult = app.addNumbers(5, 5);
+
 describe("app", () => {
-  it("should return hello", () => {
-    assert.equal(app(), "hello");
+  describe("sayHello", () => {
+    it("should return hello", () => {
+      assert.equal(sayHelloResult, "hello");
+    });
+    it("should return a string", () => {
+      assert.typeOf(sayHelloResult, "string");
+    });
+  });
+  describe("addNumbers", () => {
+    it("addNumbers should return a result greater than 5", () => {
+      assert.isAbove(addNumbersResult, 5);
+    });
+    it("addNumbers should return a number", () => {
+      assert.typeOf(addNumbersResult, "number");
+    });
   });
 });
